d7e4cde69b1b2e56ab9c3d4e7af7a0744fb3408e
abdurraheem <abdurraheem.ashraf@gmail.com>
2022-03-05 04:49:32 -0500

    removed unused code

--- src/main/java/com/springboot/webappdb/topics/TopicService.java
+++ src/main/java/com/springboot/webappdb/topics/TopicService.java
@@ -13,15 +13,7 @@ public class TopicService {
     //marks the varialble as something that needs depedency injection
     @Autowired
     private TopicRepository topicRepository;
-
-
-//    private List<Topic> topics = new ArrayList<>(Arrays.asList(
-//                new Topic("spring1", "1st other string", "Spring Framework Description1"),
-//                new Topic("spring2", "2nd other string", "Spring Framework Description2"),
-//                new Topic("spring3", "3rd other string", "Spring Framework Description3")
-//    ));
-
-
+    
     public List<Topic> getAllTopics() {
         List<Topic> topics = new ArrayList<>();
         topicRepository.findAll().forEach(topics::add);
