d25ee2e76dc61c1d4dc0829483d9e8038b52dfa3
abdurraheem <abdurraheem.ashraf@gmail.com>
2022-03-08 07:43:14 -0500

    Added some caching

--- src/main/java/com/springboot/webappdb/WebappDbApplication.java
+++ src/main/java/com/springboot/webappdb/WebappDbApplication.java
@@ -1,13 +1,34 @@
 package com.springboot.webappdb;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableCaching
 public class WebappDbApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebappDbApplication.class, args);
     }
+
+
+    public class SwaggerConfig {
+        @Bean
+
+        public OpenAPI springOpenAPI() {
+            return new OpenAPI()
+                    .info(new Info()
+                            .title("Spring API Documentation")
+                            .description("Spring Course Application")
+                            .version("v0.0.1")
+                            .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+        }
+    }
+
 
 }
--- src/main/java/com/springboot/webappdb/topics/TopicService.java
+++ src/main/java/com/springboot/webappdb/topics/TopicService.java
@@ -1,6 +1,10 @@
 package com.springboot.webappdb.topics;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,18 +12,22 @@ import java.util.Arrays;
 import java.util.List;
 
 @Service
+@CacheConfig(cacheNames = {"topics"})
 public class TopicService {
 
     //marks the varialble as something that needs depedency injection
     @Autowired
     private TopicRepository topicRepository;
-    
+
+
+    @Cacheable
     public List<Topic> getAllTopics() {
         List<Topic> topics = new ArrayList<>();
         topicRepository.findAll().forEach(topics::add);
         return topics;
     }
 
+    @Cacheable
     public Topic getTopic(String id) {
         return topicRepository.findById(id).orElse(null);
     }
@@ -35,6 +43,7 @@ public class TopicService {
     public void deleteTopic(String id) {
         topicRepository.deleteById(id);
     }
+
 }
 
 
